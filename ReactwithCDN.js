679a1e08e6af890623892861bbed7453c1a316e5
hameed <abdulhameed29r@gmail.com>
2024-12-06 17:05:00 +1100

    first commit

---
+++ ReactwithCDN.js
@@ -0,0 +1,8 @@
+const header = React.createElement(
+  "h1",
+  {},
+  "This is header created by createElement"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(header);
